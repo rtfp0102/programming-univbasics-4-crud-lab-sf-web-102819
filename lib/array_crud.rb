b3000d36339a70fa6b69f1fe000c618a983e3953
rtfp0102 <rtfp0102@gmail.com>
2019-09-29 18:04:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -38,9 +38,10 @@ end
 
 def retrieve_last_element_from_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array[]
+  array[5]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[]
 end
